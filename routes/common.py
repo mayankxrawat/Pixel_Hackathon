2892f88aacc488d819362c6f10dc723180f34f52
mayankxrawat <MAYANKRAWAT.24041069@gehu.ac.in>
2025-04-23 00:22:03 +0530

    Delete routes directory

--- routes/common.py
+++
@@ -1,8 +0,0 @@
-# routes/common.py
-from fastapi import APIRouter
-
-common_router = APIRouter()
-
-@common_router.get("/status")
-def check_status():
-    return {"status": "Faculty Work Hours Tracker is up and running!"}
